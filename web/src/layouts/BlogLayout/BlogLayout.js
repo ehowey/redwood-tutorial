60ae9fe21bb8bc1ca6bdee202fbbab44b5ae3f5f
Eric Howey <ehowey@gmail.com>
2020-04-24 21:32:52 -0600

    COntact form complete

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -7,21 +7,22 @@ import theme from './theme'
 const BlogLayout = ({ children }) => {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <header sx={{ bg: ['green', 'red', 'blue'] }}>
-          <h1>
-            <Link to={routes.home()}>Redwood Blog</Link>
-          </h1>
-          <nav>
-            <ul>
-              <li>
-                <Link to={routes.about()}>About</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <main>{children}</main>
-      </ThemeProvider>
+      <header>
+        <h1>
+          <Link to={routes.home()}>Redwood Blog</Link>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+            <li>
+              <Link to={routes.contact()}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
     </>
   )
 }
--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,9 +1,78 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+import {
+  Form,
+  TextField,
+  TextAreaField,
+  Submit,
+  FieldError,
+  Label,
+  useMutation,
+  FormError,
+} from '@redwoodjs/web'
+import { useForm } from 'react-hook-form'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+
 const ContactPage = () => {
+  const formMethods = useForm()
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      alert('Thank you for your submission!')
+      formMethods.reset()
+    },
+  })
+  const onSubmit = (data) => {
+    create({ variables: { input: data } })
+    console.log(data)
+  }
   return (
-    <div>
-      <h1>ContactPage</h1>
-      <p>Find me in ./web/src/pages/ContactPage/ContactPage.js</p>
-    </div>
+    <BlogLayout>
+      <Form
+        onSubmit={onSubmit}
+        validation={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <Label name="name" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label name="name" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+        <Submit disabled={loading}>Save</Submit>
+      </Form>
+    </BlogLayout>
   )
 }
 
